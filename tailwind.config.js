f32aa22b968a0584a5f94b4f98f0831eef20595b
krzysztofMlczk <krzysztof.malczak@ardentcode.com>
2022-11-12 17:26:51 +0100

    feat: Basic Setup
    - Add tailwind colors
    - Add PT Sans google font
    - Add app directory (experimental)
    - Add dependencies that will be used

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "chaos-black": {
+          DEFAULT: "#101010",
+        },
+        "cod-gray": {
+          DEFAULT: "#191919",
+        },
+        "dusty-gray": {
+          DEFAULT: "#979797",
+          900: "#2D2D2D",
+        },
+        supernova: {
+          DEFAULT: "#FFC700",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
